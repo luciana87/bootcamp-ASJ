ea49a573ecae152713596ff0cb59016dc9553f0d
luciana87 <ch.luciana87@gmail.com>
2023-12-13 22:05:49 -0300

    Filter completed

--- angular/my-projects/src/app/components/todo-list/todo-list.component.ts
+++ angular/my-projects/src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, booleanAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,22 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent {
-  tasks: { text: string; status: boolean }[] = [];
+  tasks: { text: string; status: boolean; deleted: boolean, created: string}[] = [];
+  filteredTaskList: { text: string; status: boolean; deleted: boolean , created: string}[] = this.tasks;
   newTask = '';
   p_date = new Date().toLocaleDateString();
+
+  value_checkbox_completed = false;
+  value_checkbox_incomplete = false;
+  value_checkbox_deleted = false;
 
   // addTask () {
   //   this.tasks.push(this.newTask);
   //   this.newTask = '';
   // }
 
+
   addTask() {
-    this.tasks.push({ text: this.newTask, status: false});
+    this.tasks.push({ text: this.newTask, status: false, deleted: false, created: new Date().toLocaleDateString()});
     this.newTask = '';
     this.p_date = new Date().toLocaleDateString();
   }
 
-  statusTask (i: number) {
+  statusTask(i: number) {
     this.tasks[i].status = !this.tasks[i].status;
+  }
+
+  deleteTask(i: number) {
+    this.tasks[i].deleted = !this.tasks[i].deleted;
+  }
+
+
+  filteredTasks() {
+    if (this.value_checkbox_completed || this.value_checkbox_incomplete || this.value_checkbox_deleted) {
+      this.filteredTaskList = this.tasks.filter(task =>
+        (this.value_checkbox_completed && task.status) ||
+        (this.value_checkbox_incomplete && !task.status) ||
+        (this.value_checkbox_deleted && task.deleted)
+      )
+
+    } else {
+      this.filteredTaskList = this.tasks;
+    }
   }
 }
